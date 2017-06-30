8920da136621e84fa52169ff120b97cf529e5dec
Ali Schlereth <aschlere@sbcglobal.net>
2017-06-30 14:23:33 -0600

    Refactor to use multiple functions

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 use std::io;
 
 fn main() {
-
     println!("HELLO, THIS IS A GROCERY STORE!");
 
-    // let mut message = String::new();
-
     let mut listening = true;
-    // let result = message == "\n";
-    // println!("{}", result);
+
+    listen(listening);
+}
 
 
+fn listen(mut listening: bool) {
     while listening {
         let mut message = String::new();
 
@@ -19,15 +18,26 @@ fn main() {
 
         let clean_message = message.trim();
 
-        if clean_message == "" {
-            println!("HELLO?!");
-        } else if clean_message == "GOODBYE!" {
-            println!("THANK YOU FOR CALLING!");
-            listening = false;
-        } else if clean_message == clean_message.to_uppercase() {
-            println!("NO, THIS IS NOT A PET STORE");
-        } else {
-            println!("I AM HAVING A HARD TIME HEARING YOU.");
-        }
+        listening = analyze_response(clean_message, listening);
     }
+}
+
+fn analyze_response(clean_message: &str, mut listening: bool) -> bool {
+    let response = if clean_message == "" {
+        "HELLO?!"
+    } else if clean_message == "GOODBYE!" {
+        listening = false;
+        "THANK YOU FOR CALLING!"
+    } else if clean_message == clean_message.to_uppercase() {
+        "NO, THIS IS NOT A PET STORE"
+    } else {
+        "I AM HAVING A HARD TIME HEARING YOU."
+    };
+
+    print_response(response);
+    listening
+}
+
+fn print_response(response: &str) {
+    println!("{}", response);
 }
